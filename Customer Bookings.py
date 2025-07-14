f58e9a57d343e16312019ddd2e216516c7df38e5
HowWilbert <bireansh1@gmail.com>
2025-07-14 19:19:01 +0530

    Update Customer Bookings.py
    The datatype of the coloumns of the primary must be same

--- Customer Bookings.py
+++ Customer Bookings.py
@@ -2,7 +2,7 @@ import mysql.connector as sql
 conn=sql.connect(host='localhost',user='root',password='mysql12',database='travel_booking')
 c1=conn.cursor()
 
-c1.execute('create table customer_bookings(Phone_number bigint(13) ,FOREIGN KEY(Phone_number) REFERENCES accounts(Phone_number),Your_location varchar(30),Your_destination varchar(30),time varchar(30),Driver varchar(60),Urgency  varchar(30),date_booked varchar(90));')
+c1.execute('create table customer_bookings(Phone_number int(13) ,FOREIGN KEY(Phone_number) REFERENCES accounts(Phone_number),Your_location varchar(30),Your_destination varchar(30),time varchar(30),Driver varchar(60),Urgency  varchar(30),date_booked varchar(90));')
 conn.commit()
 print("table  customer_bookings created successfully")
 
